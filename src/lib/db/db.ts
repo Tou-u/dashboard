43f65e9aff8e055386ae50f71e9ec036f34747b5
Tou-u <rodrignacio99@hotmail.com>
2024-01-28 02:04:35 -0300

    local db

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,9 +2,21 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
+// Production
 const url = process.env.TURSO_URL;
 const authToken = process.env.TURSO_TOKEN;
 
-if (!url || !authToken) throw new Error("Turso credentials are not defined");
+// Development
+const localdb = process.env.DATABASE_URL
 
-export const db = drizzle(createClient({ url, authToken }), { schema });
+let client = null
+
+if(url && authToken) {
+    client = createClient({url, authToken})
+} else if (localdb) {
+    client = createClient({url: localdb})
+} else {
+    throw new Error('Credentials not provided')
+}
+
+export const db = drizzle(client, {schema})
